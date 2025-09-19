a129fac513583e8f9845d5689d8df00dd32a7b41
anithepani <aryanzia2005@gmail.com>
2025-09-19 10:57:13 +0500

    Analyze Pointer Program

---
+++ analyzepointer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+void analyze_pointer(int *ptr){
+    cout << "The memory location the pointer points to :";
+    cout << ptr <<endl;
+    cout << "The value stored inside the memory location which the pointer points to :";
+    cout << *ptr<<endl;
+}
+int main(){
+    int iValue_stack = 7;
+    int *pointer_stack = &iValue_stack;
+    analyze_pointer(pointer_stack);
+    int *iValue_heap = new int[7];
+    analyze_pointer(iValue_heap);
+
+delete iValue_heap; 
+return 0;
+
+}
